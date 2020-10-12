9cf2608d4581d4eaf494b3b6a1f1da7c4bb7babf
ChevaunBent <chevaunbent@gmail.com>
2020-10-12 01:23:26 -0500

    Exercise 3 modified

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,20 @@
 "use strict";
+const divEl= document.querySelectorAll("#board div");
 const restartButton = document.getElementsByClassName('btn');
+const X_Class = 'X'
+const O_Class = 'O'
 var circleTurn= false;
-let state=[];
+const WIN_COMB = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+
 
 
 
@@ -14,7 +27,6 @@ window.onload = function(){
             event.target.classList.remove("hover");
             event.target.classList.add("hover O");
         }
-            /* event.target: parent element */
           };
     });
 
@@ -22,13 +34,17 @@ window.onload = function(){
         circleTurn = !circleTurn
       }    
 
+    function checkWin(){
+
+    }
+
     document.querySelectorAll("#board div").forEach(item => {
         item.addEventListener('click', event => {
           if(circleTurn){
-            item.innerHTML = "O";
+            item.innerHTML = O_Class;
             item.setAttribute("class", "square O")
           }else{
-            item.innerHTML = "X";
+            item.innerHTML = X_Class;
             item.setAttribute("class", "square X")
           }
         swapTurns();  
