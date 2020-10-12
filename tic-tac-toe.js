a0200136a851c342dc6d3e057230d32dd3173dfd
ChevaunBent <chevaunbent@gmail.com>
2020-10-12 00:09:29 -0500

    Exercise 2 completed

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,35 @@
 "use strict";
+const restartButton = document.getElementsByClassName('btn');
+var circleTurn= true;
+let state=[];
+
+
+
 window.onload = function(){
     document.querySelectorAll("#board div").forEach(function(divs){
         divs.setAttribute("class", "square");
     });
+
+    function swapTurns() {
+        circleTurn = !circleTurn
+      }    
+
+    document.querySelectorAll("#board div").forEach(item => {
+        item.addEventListener('click', event => {
+          if(circleTurn){
+            item.innerHTML = "O";
+            item.setAttribute("class", "square O")
+          }else{
+            item.innerHTML = "X";
+            item.setAttribute("class", "square X")
+          }
+        swapTurns();  
+
+        })
+      })
+
+
+
 }
 
 
